2e540f282c5f8e3b7c32518a61d3ca41485f39f6
Pablo Eduardo Moreira <95319664+pabloedduarddo@users.noreply.github.com>
2023-04-11 20:53:22 -0300

    Update DAO.java

--- src/model/DAO.java
+++ src/model/DAO.java
@@ -4,22 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DAO {
-	// Variáveis para configurar o banco de dados
+	
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://10.26.49.203:3306/dbagenda";
 	private String user = "dba";
 	private String password = "123@senac";
-	// Criação de um objeto para uso da classe Connection (JDBC)
+	
 	private Connection con;
-	/**
-	 * Método responsável pela conexão com o banco
-	 * @return con
-	 */
+	
 	
 	public Connection conectar() {
-		// tratamento de exceções
 		try {
-			// as linhas abaixo abrem a conexão com o banco
+			
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, user, password);
 			return con;
